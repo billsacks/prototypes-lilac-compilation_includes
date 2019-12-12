9ff4d94807edf2d3a915401a75b7b9d763e9894f
Bill Sacks <sacks@ucar.edu>
2019-12-12 12:02:36 -0700

    Demonstrate link issues

--- shr_kind_dependency/atm/atm.f90
+++ shr_kind_dependency/atm/atm.f90
@@ -20,10 +20,26 @@ contains
   end subroutine atm_driver
 end module atm
 
+module ctsm
+  implicit none
+  private
+
+  public :: ctsm_in
+
+contains
+
+  subroutine ctsm_in(x)
+    integer, intent(in) :: x
+
+    print *, 'atm driver ctsm_in: ', x
+  end subroutine ctsm_in
+end module ctsm
 
 program atm_program
   use atm, only : atm_driver
+  use ctsm, only : ctsm_in
   implicit none
 
   call atm_driver()
+  call ctsm_in(42)
 end program atm_program
