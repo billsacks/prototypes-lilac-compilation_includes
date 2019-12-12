8aec4b668064d703aa51446bd54ddc927c1363b5
Bill Sacks <sacks@ucar.edu>
2019-12-12 05:46:13 -0700

    Add lilac subdirectory

---
+++ shr_kind_dependency/lilac/lilac.f90
@@ -0,0 +1,16 @@
+module lilac
+
+  use shr_kind_mod, only : r8
+  use ctsm, only : call_ctsm
+  implicit none
+
+  public :: call_lilac
+
+contains
+
+  subroutine call_lilac(x)
+    real(r8) :: x
+
+    call call_ctsm(x)
+  end subroutine call_lilac
+end module lilac
